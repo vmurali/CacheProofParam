07ba2f7391ae2072e1e6d9180fde7e69c5949d88
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2014-02-26 23:06:53 -0500

    defined resp

--- CacheLocal.v
+++ CacheLocal.v
@@ -16,7 +16,29 @@ Module mkCacheLocal (coh: Coherence).
   Record CacheLocal :=
     {
       getCacheState: Time -> CacheState;
-      deqR: Proc -> Time -> Prop
+      deqR: Proc -> Time -> Prop;
+      deqOrNot: forall t, {p| deqR p t} + {forall p, ~ deqR p t}
     }.
 
+  Section CacheLocal.
+    Variable cl: CacheLocal.
+    Variable t: Time.
+    Definition s := getCacheState cl t.
+
+    Definition respFn :=
+      match deqOrNot cl t with
+        | inleft someDeq =>
+          match someDeq with
+            | exist pCache _ =>
+              let p := p_node pCache in
+              let (a, op, _) := reqFn pCache (next s p) in
+              Some (Build_Resp pCache (next s p)
+                               match op with
+                                 | Ld => data s p a
+                                 | St => initData zero
+                               end)
+          end
+        | inright _ => None
+      end.
+  End CacheLocal.
 End mkCacheLocal.
